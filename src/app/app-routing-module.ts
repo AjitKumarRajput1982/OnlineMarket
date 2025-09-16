1a2e3791a8eb3e3558eea63b9ca011da94da43b4
Ajit Kumar <ajitkumarrajput1982@gmail.com>
2025-09-16 13:18:15 +0530

    Code Added

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,8 +1,22 @@
-import { NgModule } from '@angular/core';
+import { createComponent, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Motorcycles } from '../motorcycles/motorcycles';
+import { Cars } from '../cars/cars';
+import { MobilePhones } from '../mobile-phones/mobile-phones';
+import { SaleHouseAppartment } from '../sale-house-appartment/sale-house-appartment';
+import { Scooters } from '../scooters/scooters';
+import { OtherVehicle } from '../other-vehicle/other-vehicle';
+import { RentHouseAppartment } from '../rent-house-appartment/rent-house-appartment';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path:"cars",component:Cars},
+  {path:"motorcycles",component:Motorcycles},
+  {path:"mobile-phones",component:MobilePhones},
+  {path:"sale-house-appartment",component:SaleHouseAppartment},
+  {path:"scooters",component:Scooters},
+  {path:"other-vehicle",component:OtherVehicle},
+   {path:"rent-house-appartment",component:RentHouseAppartment}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
