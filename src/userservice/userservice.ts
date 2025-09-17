468a1ebd9aef2541f56ef51a00e0dbad19449933
Ajit Kumar <ajitkumarrajput1982@gmail.com>
2025-09-17 23:00:38 +0530

    Added code

--- src/userservice/userservice.ts
+++ src/userservice/userservice.ts
@@ -17,26 +17,26 @@ export class Userservice {
 
   getcarbrand():Observable<Carbrand[]>
   {
-    return this.httpClient.get<Carbrand[]>("api/getcarbrand");
+    return this.httpClient.get<Carbrand[]>("https://localhost:7020/api/car/getcarbrand");
   }  
   getcarbudget():Observable<Carbudget[]>
   {
-    return this.httpClient.get<Carbudget[]>("api/getcarbudget");
+    return this.httpClient.get<Carbudget[]>("https://localhost:7020/api/car/getcarbudget");
   }  
   getcarfuel():Observable<Carfuel[]>
   {
-    return this.httpClient.get<Carfuel[]>("api/getcarfuel");
+    return this.httpClient.get<Carfuel[]>("https://localhost:7020/api/car/getcarfuel");
   } 
   getcarkmdriven():Observable<Carkmdriven[]>
   {
-    return this.httpClient.get<Carkmdriven[]>("api/getcarkmdriven");
+    return this.httpClient.get<Carkmdriven[]>("https://localhost:7020/api/car/getcarkmdriven");
   } 
   getcarnoofowner():Observable<Carnoofowner[]>
   {
-    return this.httpClient.get<Carnoofowner[]>("api/getcarnoofowner");
+    return this.httpClient.get<Carnoofowner[]>("https://localhost:7020/api/car/getcarnoofowner");
   } 
   getcarstate():Observable<State[]>
   {
-    return this.httpClient.get<State[]>("api/getstate");
+    return this.httpClient.get<State[]>("https://localhost:7020/api/car/getcarstate");
   } 
 }
